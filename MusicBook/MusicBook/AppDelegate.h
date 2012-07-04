4297efdc5a645fabfee485824b2d3a887e2d9338
Tom Callon <tomcallon2010@gmail.com>
2012-07-04 20:06:28 +0800

    ... correct message ...

---
+++ MusicBook/MusicBook/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  MusicBook
+//
+//  Created by Tom Callon (Hotmail) on 7/4/12.
+//  Copyright (c) 2012 __MyCompanyName__. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate, UITabBarControllerDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@property (strong, nonatomic) UITabBarController *tabBarController;
+
+@end
---
+++ MusicBook/MusicBook/SecondViewController.h
@@ -0,0 +1,13 @@
+//
+//  SecondViewController.h
+//  MusicBook
+//
+//  Created by Tom Callon (Hotmail) on 7/4/12.
+//  Copyright (c) 2012 __MyCompanyName__. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface SecondViewController : UIViewController
+
+@end
