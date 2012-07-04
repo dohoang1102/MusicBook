f4f64e1cee09f20bd2bcb9b495f623450424db73
TomCallonOld <tomcallon2010@yahoo.com>
2012-07-04 21:51:18 +0800

    this is Tom Callon do you think so man .

--- MusicBook/MusicBook/SecondViewController.m
+++ MusicBook/MusicBook/SecondViewController.m
@@ -28,6 +28,7 @@
 {
     [super viewDidLoad];
 	// Do any additional setup after loading the view, typically from a nib.
+    NSLog(@"ow afdasdfas;dfasjdfas;dfasdflasdfjjl;fs");
 }
 
 - (void)viewDidUnload
