45c589f98bc61c706909d68666176cf01bfbb232
Tom Callon <tomcallon2010@gmail.com>
2012-07-06 14:40:55 +0800

    qwertyuio

--- MusicBook/MusicBook/SecondViewController.m
+++ MusicBook/MusicBook/SecondViewController.m
@@ -19,7 +19,7 @@
     self = [super initWithNibName:nibNameOrNil bundle:nibBundleOrNil];
     if (self) {
         self.title = NSLocalizedString(@"Second", @"Second");
-        self.tabBarItem.image = [UIImage imageNamed:@"second"];
+        self.tabBarItem.image = [UIImage imageNamed:@"C01-1"];
         NSLog(@"ghjkl");   
     }
     return self;
